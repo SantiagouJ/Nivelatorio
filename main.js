fdd0251bde9ec82066cc839b788159970dd41a96
SantiagouJ <santiagojuradorodriguez3@gmail.com>
2025-02-21 18:07:19 -0500

    Entreaga

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 const studentGrades = [100, 50, 65, 70, 85, 96, 45];
-// const result = computeGrades(studentGrades)
-// console.log(result)
+const result = computerGrades(studentGrades)
+console.log(result)
 
-let approved = 0
-let reproved = 0
+var approved = 0
+var reproved = 0
 
-function computerGrades(notas = [100, 50, 65, 70, 85, 96, 45]){
+function computerGrades(){
 
     if(notas>=90){
         console.log("gola")
+        approved++
     }else if(notas>=80 && notas<=89) {
         console.log("oliwiza")
+        approved++
     }else if(notas>=70 && notas<=79){
         console.log("f")
+        approved++
     }else if(notas>=60 && notas<=69){
         console.log("terri")
+        reproved++
     }
-    
 
+    console.log(approved, reproved)
 }
 
-// const result = computerGrades(studentGrades)
-// console.log(result)
+console.log(objetico.nota1[0].nota2)
 
-console.log("hls")
-console.log(computerGrades())
+const finalGrades = {
+    parsed: [],
+    passed: 0,
+    lose: 0
+
+}
